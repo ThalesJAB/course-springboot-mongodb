831081fd576ede38d6d50c592316164ef5633e49
Thales José Aguiar Batista <thalesjoseaguiar@gmail.com>
2021-07-05 22:39:37 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/educandoweb/course/workshopmongo/CourseSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringbootMongodbApplication.class, args);
+	}
+
+}
